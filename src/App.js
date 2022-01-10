38c17e27d1799935589e5697ff2825cd932764f7
aquiwer <70368630+aquiwer@users.noreply.github.com>
2022-01-10 21:46:20 +0200

    Build 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import {useWindowSize} from '@react-hook/window-size'
+import Confetti from 'react-confetti'
+import Snowfall from 'react-snowfall'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export const App = () => {
+    const [seconds, setSeconds] = useState(10);
+    const [minutes, setMinutes] = useState(0);
+
+    const {width, height} = useWindowSize()
+    const [isShowAnim, setIsShowAnim] = useState(false);
+
+    useEffect(() => {
+        let myInterval = setInterval(() => {
+            if (seconds > 0) {
+                setSeconds(seconds - 1);
+            }
+            if (seconds === 0) {
+                setMinutes(minutes - 1)
+                setSeconds(60)
+            }
+            if (minutes === 0) {
+
+                if (seconds === 0) {
+                    clearInterval(myInterval);
+                    setSeconds(0)
+                    setMinutes(0)
+                }
+            }
+        }, 1000);
+        return () => {
+            clearInterval(myInterval);
+        };
+    });
+    useEffect(() => {
+        if (seconds === 0 && minutes === 0) {
+            setIsShowAnim(true);
+        }
+    }, [seconds])
+    return (
+        <div className="counter-block">
+            <Snowfall/>
+            <div className='counter-info-block'>
+                {
+                    isShowAnim && <div>
+                        <div>
+                            <h1 className='counter-title'>Happy birthday, my dear Alina! ❤️</h1>
+                        </div>
+                    </div>
+                }
+
+                <div className='counters-block'>
+                    <div className="timer_section-count">
+                        <p>{minutes >= 10 ? minutes.toString().split().join()[0] : 0}</p></div>
+                    <div className="timer_section-count">
+                        <p>{minutes >= 10 ? minutes.toString().split().join()[1] : minutes.toString().split().join()[0]}</p>
+                    </div>
+                    <div className="timer_section-count"><p>:</p></div>
+                    <div className="timer_section-count">
+                        <p>{seconds >= 10 ? seconds.toString().split().join()[0] : 0}</p></div>
+                    <div className="timer_section-count">
+                        <p>{seconds >= 10 ? seconds.toString().split().join()[1] : seconds.toString().split().join()[0]}</p>
+                    </div>
+                </div>
+            </div>
+
+            <Confetti
+                run={isShowAnim}
+                width={width}
+                height={height}
+            />
+
+        </div>
+    );
 }
 
-export default App;
